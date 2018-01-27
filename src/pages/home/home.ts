1f9a9b676942a385dd3612f5d2301bbbd34206a5
Ryan Miller <ryan@ryanandrewmiller.com>
2018-01-27 13:42:58 -0800

    Add login functionality.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component, NgZone } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import CCCredentials from '../../credentials';
 import { Platform } from 'ionic-angular/platform/platform';
+import { LoadingController } from 'ionic-angular/components/loading/loading-controller';
 
 @Component({
   selector: 'page-home',
@@ -10,29 +11,70 @@ import { Platform } from 'ionic-angular/platform/platform';
 export class HomePage {
   
   initializing = true
+  initializationError = null
+
+  username = ''
+  password = ''
+
+  loggedIn = false
 
   constructor(
+    public loadingCtrl: LoadingController,
     public navCtrl: NavController,
     public plt: Platform,
     public zone: NgZone
   ) { }
 
-  ionViewWillEnter() {
-    // See https://docs.cometchat.com/cordova-ionic-plugin/initialization/
+  initializeCometChat(success, error) {
     window['CCCometChat'].initializeCometChat(
       CCCredentials.cometchat_domain,
       CCCredentials.cometchat_license_key,
       CCCredentials.cometchat_api_key, 
       this.plt.is('ios') ? 'NO' : false,
-      () => { 
-        this.initializing = false
-        this.zone.run(() => {});
-        console.log('CometChat has initialized!') 
-      }, function (err) { 
-        console.error('Error while initializing CometChat...')
-        console.error(err) 
-      }
+      success, error
     )
+  }
+
+  getCometChatInstance(success, error) {
+    window['CCCometChat'].getInstance(success, error)
+  }
+
+  ionViewWillEnter() {
+    // See https://docs.cometchat.com/cordova-ionic-plugin/initialization/
+    if (this.plt.is('ios')) {
+      this.initializeCometChat(
+        () => { 
+          console.log('CometChat has initialized!') 
+          this.initializing = false
+          this.zone.run(() => {});
+        }, function (err) { 
+          console.error('Error while initializing CometChat...')
+          this.initializing = false
+          this.initializationError = err;
+          this.zone.run(() => {});
+        }
+      )
+    } else {
+      this.getCometChatInstance(() => {
+        this.initializeCometChat(
+          () => { 
+            console.log('CometChat has initialized!') 
+            this.initializing = false
+            this.zone.run(() => {});
+          }, function (err) { 
+            console.error('Error while initializing CometChat...')
+            this.initializing = false
+            this.initializationError = err;
+            this.zone.run(() => {});
+          }
+        )
+      }, (err) => {
+        console.error('Error getting CometChat instance...')
+        this.initializing = false
+        this.initializationError = err
+        this.zone.run(() => {});
+      })
+    }
   } 
 
   launchCometChat() {
@@ -43,6 +85,24 @@ export class HomePage {
       }, function (err) { 
         console.error('Error while launching CometChat...')
         console.error(err) 
+      }
+    )
+  }
+
+  login() {
+    const loader = this.loadingCtrl.create();
+
+    loader.present()
+
+    window['CCCometChat'].login(this.username, this.password,
+      () => { 
+        this.loggedIn = true
+        this.zone.run(() => {});
+        loader.dismiss()
+      }, function (err) { 
+        alert('Please try again')
+        this.zone.run(() => {});
+        loader.dismiss()
       }
     )
   }
