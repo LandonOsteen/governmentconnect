4a3b99dee22353140e776c9bdf75a24e27d812ea
Ryan Miller <ryan@ryanandrewmiller.com>
2018-02-17 10:13:39 -0800

    Add draft of login page.

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, AlertController } from 'ionic-angular';
 import { AngularFireAuth } from 'angularfire2/auth';
 
+@IonicPage()
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
@@ -12,6 +13,7 @@ export class LoginPage {
   password: string = ''
 
   constructor(
+    public alertCtrl: AlertController,
     public navCtrl: NavController,
     public firebaseAuth: AngularFireAuth
   ) { }
@@ -20,9 +22,19 @@ export class LoginPage {
     try {
       const user = await this.firebaseAuth.auth.signInAndRetrieveDataWithEmailAndPassword(this.email, this.password)
 
-      console.log(user)
+      const alert = this.alertCtrl.create({
+        title: 'Success!',
+        message: 'Those credentials are valid!'
+      })
+
+      alert.present()
     } catch (err) {
-      console.log(err)
+      const alert = this.alertCtrl.create({
+        title: 'Hmm...',
+        message: 'Those credentials are invalid...'
+      })
+
+      alert.present()
     }
   }
 }
